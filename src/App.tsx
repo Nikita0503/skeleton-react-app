1db601055936a253314a222b383eff0740112893
Nikita0503 <nikita0503ua@gmail.com>
2026-06-09 14:56:41 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <h1 className={styles.title}>Travel Agency</h1>
+      <p className={styles.subtitle}>Skeleton is ready</p>
+    </div>
+  );
+}
+
+export default App;
